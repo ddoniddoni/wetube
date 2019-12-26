8e000da47f4bc02cb44207b294af65b1ba188565
DDoni <psdkei@naver.com>
2019-12-26 19:09:57 +0900

    add Webpack

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+
 const app = express();
 
 app.use(helmet()); // application이 더 안전하도록 만들어주는 미들웨어
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads")); // 주어진 directory에서 file을 전달하는 미들웨어
+app.use("/static", express.static("static"));
 app.use(cookieParser()); // cookie 를 전달받아서 사용할 수 있도록 만들어주는 미들웨어
 app.use(bodyParser.json()); // 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어
 app.use(bodyParser.urlencoded({ extended: true })); // 위와 동일
